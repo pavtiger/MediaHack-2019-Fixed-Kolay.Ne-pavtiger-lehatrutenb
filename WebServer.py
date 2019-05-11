9329a7d9a3a696c6ab229614951352f13df07b4a
Pavel Artushkov <pavtiger@gmail.com>
2019-05-11 18:43:46 +0300

    NoErrors

--- WebServer.py
+++ WebServer.py
@@ -1,9 +1,9 @@
-from flask import Flask
+import flask
 import json
-from news_analyzer import get_popular_tags()
+from news_analyzer import get_popular_tags
+from f9 import get_filtered_news, get_filtered_public_posts
 
-
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
@@ -11,24 +11,24 @@ def index():
 
 
 @app.route('/get_popular_tags', methods=['POST'])
-def get_popular_tags():
+def get_popular_tags_():
     return json.dumps(get_popular_tags())
 
 @app.route('/get_filtered_news', methods=['POST'])
-def page_name():
-    offset = flask.form['offset']
-    token = flask.form['token']
-    count = flask.form['count']
-    tags = flask.form['tags']
+def get_filtered_news_():
+    offset = flask.request.form['offset']
+    token = flask.request.form['token']
+    count = flask.request.form['count']
+    tags = flask.request.form['tags']
     return json.dumps(get_filtered_news(token=token, offset=offset, count=count, tags=tags))
 
 @app.route('/get_filtered_public_posts', methods=['POST'])
-def page_name():
-    offset = flask.form['offset']
-    tok = flask.form['token']
-    count = flask.form['count']
-    public_id = flask.form['public_id']
-    tags = flask.form['tags']
-    return json.dumps(get_public_posts(token=token, offset=offset, count=count, tags=tags, public_id=public_id))
+def get_filtered_public_posts_():
+    offset = flask.request.form['offset']
+    token = flask.request.form['token']
+    count = flask.request.form['count']
+    public_id = flask.request.form['public_id']
+    tags = flask.request.form['tags']
+    return json.dumps(get_filtered_public_posts(token=token, offset=offset, count=count, tags=tags, public_id=public_id))
 
 app.run('0.0.0.0', port=5000)
