3f89c9e2e9ebc921c53c47fc8bf8ab27fd9901ca
lehatrutenb <36619154+lehatrutenb@users.noreply.github.com>
2019-05-11 16:43:58 +0300

    update

--- f9.py
+++ f9.py
@@ -3,12 +3,16 @@ import random
 import numpy as np
 import requests
 from pprint import pprint
+import urllib
+from text_analyzer import check_text
+from image_analyzer import get_image_tags
 
-class Bot:
+class f9:
     def __init__(self):
         n = 0
         n += 1
     def get_filtered_news(self, tokken, offset, count, tags):
+        tr = True
         try:
             vk = vk_api.VkApi(token=tokken)
             m2 = vk.method('newsfeed.get', {'count':count + offset, 'filters':'post'})
@@ -16,10 +20,15 @@ class Bot:
             text = m2['items'][((count + offset) % 100) - 1]['text']
         except:
             vk = vk_api.VkApi(token=tokken)
+            tr = False
             m2 = vk.method('newsfeed.get', {'count': count + offset, 'filters': 'post'})
             #pprint(m2['items'][((count + offset) % 100) - 1]['attachments'][0])
             return 'bad request'
+        if(tr):
+            check_text(text)
+            get_image_tags(img_url)
     def public_posts(self, tokken, offset, count, tags, pub_id):
+        tr = True
         try:
             vk = vk_api.VkApi(token=tokken)
             m2 = vk.method('newsfeed.get', {'count':count + offset, 'filters':'post', 'source_ids': '-'+str(pub_id)})
@@ -27,9 +36,13 @@ class Bot:
             text = m2['items'][((count + offset) % 100) - 1]['text']
         except:
             vk = vk_api.VkApi(token=tokken)
+            tr = False
             m2 = vk.method('newsfeed.get', {'count': count + offset, 'filters': 'post'})
             #pprint(m2['items'][((count + offset) % 100) - 1]['attachments'][0])
             return 'bad request'
+        if(tr):
+            check_text(text)
+            get_image_tags(img_url)
 #gl = Bot()
 #tokken = '35db77277a6e00ea8035db2316ed190de24ca77c162c958eb72e83f70cc765b9a4d48dab35662aebfd65a'
 #gl.public_posts(tokken, 0, 1, ['fd', 'fg', 'tr'], 'tnull')
