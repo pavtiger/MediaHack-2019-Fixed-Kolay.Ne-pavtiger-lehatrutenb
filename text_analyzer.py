05fd8e5855c7551f7f981ccdfa32ff8c61c5ae55
Pavel Artushkov <pavtiger@gmail.com>
2019-05-11 14:58:53 +0300

    Text analyzer now works

--- text_analyzer.py
+++ text_analyzer.py
@@ -15,8 +15,8 @@ def check_text(txt, tags):
         tagLen = tagWord.count(' ')
         for i in range(len(txt) - (tagLen + 1)):
             summ += [0, 1][tagWord == toKind(' '.join(txt[i:i + tagLen + 1]))]
-    print(summ)
+    return summ >= 1
 
 if __name__ == "__main__":
     strr = "В последнее время редактор XDA Developers, Мишал Рахмана, проделал большую работу, выискивая кодовые имена и краткие спецификации предполагаемых смартфонов и планшетов. Недавно он натолкнулся на несколько новых устро//йств Huawei, и теперь у него есть пара из"
-    check_text(strr, ['XDA Developers', 'Мишал Рахмана'])
+    print(check_text(strr, ['XзA Developers', 'Мышал Рахмана']))
