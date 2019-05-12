f04cd7723945180c7e630b9907c312c5e6791916
FixedOctocat <egorfilatov2002@yandex.ru>
2019-05-12 13:06:37 +0300

    image_analyzer add translation

--- image_analyzer.py
+++ image_analyzer.py
@@ -3,7 +3,14 @@ from pytesseract import image_to_string
 import io
 from google.cloud import vision
 from google.cloud.vision import types
+from google.cloud import translate
+from google.cloud import translate
 from text_analyzer import check_text
+
+def translation(text, target='ru'):
+    translate_client = translate.Client()
+    translation = translate_client.translate(text, target_language=target)
+    return translation['translatedText']
 
 def annotate(path):
     client = vision.ImageAnnotatorClient()
@@ -22,13 +29,12 @@ def report(annotations):
 
     if annotations.web_entities:
         for entity in annotations.web_entities:
-            ret[entity.description] = entity.score
+            ret[translation(entity.description)] = entity.score
 
     return ret
 
 def get_text_on_image(img):
     image = Image.open(img, mode='r')
-    
     return image_to_string(image, lang='eng')
 
 def check_picture(img, tag):
@@ -36,17 +42,21 @@ def check_picture(img, tag):
     Returns: True if image have
     ratio with tags,
     False if not """
-
-    text_on_image = get_text_on_image(img)
+    
+    text_on_image = translation(get_text_on_image(img))
     if(check_text(text_on_image, tag)):
         return True
     web_entries = report(annotate(img))
-    if(check_text(' '.join(web_entires))):
+    if(check_text(' '.join(web_entries), tag)):
         return True
     return False
 
 if __name__ == '__main__':
-    img = 'tyan.jpg'
+    img = 'got1.jpg'
+
+    check_picture(img, tag=['Game of '])
 
     text = get_text_on_image(img)
     web_entries = report(annotate(img))
+
+    print(web_entries)
