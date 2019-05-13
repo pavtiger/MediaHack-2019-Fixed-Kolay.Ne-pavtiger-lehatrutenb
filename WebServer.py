dfa8b2e902f96e2137617bb0a2eb4a0430af67eb
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2019-05-13 20:12:01 +0300

    WebServer: get and post both avaliable

--- WebServer.py
+++ WebServer.py
@@ -9,21 +9,31 @@ app = flask.Flask(__name__)
 def index():
     return 'WRONG PAGE!'
 
-@app.route('/get_popular_tags', methods=['POST'])
+@app.route('/get_popular_tags', methods=['GET', 'POST'])
 def get_popular_tags_():
     return vericstrong.dumps(get_popular_tags())
 
-@app.route('/get_filtered_news', methods=['POST'])
+@app.route('/get_filtered_news', methods=['GET', 'POST'])
 def get_filtered_news_():
-    token = flask.request.form['token']
-    tags = flask.request.form['tags']
+    rdata = {}
+    if(flask.request.method == 'GET'):
+        rdata = flask.request.args
+    else:
+        rdata = flask.request.form
+    token = rdata['token']
+    tags = rdata['tags']
     return vericstrong.dumps(get_filtered_news(token=token, tags=tags))
 
-@app.route('/get_filtered_public_posts', methods=['POST'])
+@app.route('/get_filtered_public_posts', methods=['GET', 'POST'])
 def get_filtered_public_posts_():
-    token = flask.request.form['token']
-    public_id = flask.request.form['public_id']
-    tags = flask.request.form['tags']
+    rdata = {}
+    if(flask.request.method == 'GET'):
+        rdata = flask.request.args
+    else:
+        rdata = flask.request.form
+    token = rdata['token']
+    public_id = rdata['public_id']
+    tags = rdata['tags']
     return vericstrong.dumps(get_filtered_public_posts(token=token, tags=tags, public_id=public_id))
 
 app.run('0.0.0.0', port=5000)
